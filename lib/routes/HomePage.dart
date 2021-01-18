2f7c8a456c4b6e508a1729982bb696d6616d7587
lixiaohu <18237486200@163.com>
2021-01-18 15:58:19 +0800

    实现组件和数据中心的链接

--- lib/routes/HomePage.dart
+++ lib/routes/HomePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/BottomNavWidget.dart';
 import 'package:city_pickers/city_pickers.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import '../common/store.dart' as BaseStore;
 
 class City {
   City(this.cityId, this.cityName);
@@ -21,86 +23,96 @@ class HomePageState extends State<HomePage> {
   City city;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // 首页标题书写
-      appBar: AppBar(
-        title: ConstrainedBox(
-          constraints: BoxConstraints(
-            minWidth: double.infinity,
-            minHeight: 50.0,
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                left: 5,
-                child: GestureDetector(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        city != null ? city.cityName : '深圳市',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
+    // store 初始化配置
+    return new StoreProvider(
+      store: BaseStore.AppStore,
+      child: Scaffold(
+        // 首页标题书写
+        appBar: AppBar(
+          title: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: double.infinity,
+              minHeight: 50.0,
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  left: 5,
+                  child: new StoreConnector(builder: (context, calback) {
+                    return GestureDetector(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            city != null ? city.cityName : '深圳市',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Icon(Icons.arrow_drop_down),
+                        ],
                       ),
-                      Icon(Icons.arrow_drop_down),
-                    ],
-                  ),
-                  onTap: () async {
-                    print('选中城市');
-                    // 城市选中实例化
-                    Result result = await CityPickers.showCityPicker(
-                      context: context,
-                      showType: ShowType.pc,
-                      cancelWidget: Text(
-                        '取消',
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      confirmWidget: Text(
-                        '确定',
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      height: 260,
+                      onTap: () async {
+                        print('选中城市');
+                        // 城市选中实例化
+                        Result result = await CityPickers.showCityPicker(
+                          context: context,
+                          showType: ShowType.pc,
+                          cancelWidget: Text(
+                            '取消',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          confirmWidget: Text(
+                            '确定',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          height: 260,
+                        );
+                        if (result != null) {
+                          setState(() {
+                            city = City(result.cityId, result.cityName);
+                            calback();
+                          });
+                        }
+                      },
                     );
-                    if (result != null) {
-                      setState(() {
-                        city = City(result.cityId, result.cityName);
-                      });
-                    }
-                  },
+                  }, converter: (store) {
+                    return () => store.dispatch(
+                        BaseStore.LocationAction(payload: city.cityName));
+                  }),
                 ),
-              ),
-              Container(
-                child: Center(
-                  child: Transform.translate(
-                    offset: Offset(25, 0),
-                    child: Text(widget.title),
+                Container(
+                  child: Center(
+                    child: Transform.translate(
+                      offset: Offset(25, 0),
+                      child: Text(widget.title),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print('点击查询');
+              },
+            )
+          ],
+          centerTitle: true,
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('点击查询');
-            },
-          )
-        ],
-        centerTitle: true,
-      ),
-      bottomNavigationBar: BottomNavWidget(
-        select: 0,
-      ),
-      body: Container(
-        child: Text('首页'),
+        bottomNavigationBar: BottomNavWidget(
+          select: 0,
+        ),
+        body: Container(
+          child: Text('首页'),
+        ),
       ),
     );
   }
